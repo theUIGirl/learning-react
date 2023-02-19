85053d2d8b8b941e4e89b8d02311877512afcd2c
theUIGirl <125808004+theUIGirl@users.noreply.github.com>
2023-02-19 17:18:32 +0000

    first react program

---
+++ App.js
@@ -0,0 +1,19 @@
+const heading = React.createElement(
+    "h1",
+    { id: "title" },
+    "Namastey All!!"
+  );
+  const heading2 = React.createElement(
+    "h2",
+    { id: "title" },
+    "How are you doing?"
+  );
+  const container = React.createElement(
+    "div",
+    { id: "container" },
+    [heading, heading2]
+  );
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  root.render(container);
